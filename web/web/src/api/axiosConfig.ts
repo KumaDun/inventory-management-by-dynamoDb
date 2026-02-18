3c4bb5d6882e93c15b0d963b06e6d750caf3f608
KumaDun <luolanziying@hotmail.com>
2026-02-18 14:02:57 +0800

    Change the api url to query AWS EB

--- web/web/src/api/axiosConfig.ts
+++ web/web/src/api/axiosConfig.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    // Local
+    // baseURL: "http://localhost:5000/api",
+    // Online
+    baseURL: "http://inventory-management-system-env.eba-fiteiyeq.us-east-1.elasticbeanstalk.com/api",
     headers: {
         "Content-Type": "application/json",
     }
