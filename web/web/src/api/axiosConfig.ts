f80db40e5f42167a7489ea321d1f99939736f6bb
KumaDun <luolanziying@hotmail.com>
2026-02-27 15:55:11 +0800

    reopen online url

--- web/web/src/api/axiosConfig.ts
+++ web/web/src/api/axiosConfig.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
     // Local
-    baseURL: "http://localhost:5000/api",
+    // baseURL: "http://localhost:5000/api",
     // Online
-    // baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     }
